71871d28e05ebf34a4bd9f9f499e6e258070f9c4
Chava Harish <harishchava66@gmail.com>
2017-10-30 13:41:59 +0530

    with nav-bar & footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { CardImageComponent } from './card-image/card-image.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    CardImageComponent
+    CardImageComponent,
+    FooterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
